beb609e13731c0ae9e99b3697f60c239a2c679ef
Neha Dantuluri <139188813+ndantuluri429@users.noreply.github.com>
2026-01-03 15:47:18 -0500

    Rename file, update code comments
    rename start.terminal.sh -> start_terminal.sh. Added note for user to update their UID accordingly with line 3.

--- start_terminal.sh
+++ start_terminal.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 echo "docker exec -u mfm:mfm -it "mfm-$USER" /bin/bash"
-docker exec -u mfm:1000 -it "mfm-$USER" /bin/bash
-
-
+docker exec -u mfm:501 -it "mfm-$USER" /bin/bash # edit UID
